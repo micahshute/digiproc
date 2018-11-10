c5f5fa1ed4eb7bb93954efc94cd55ffdb486d7b0
micahshute <micah.shute@gmail.com>
2018-11-10 02:07:34 -0600

    removed unnecessary methods from ifft strategy

--- lib/strategies/fft/inverse_fft_conjugate_strategy.rb
+++ lib/strategies/fft/inverse_fft_conjugate_strategy.rb
@@ -15,19 +15,6 @@ class Dsp::Strategies::IFFTConjugateStrategy
         conjugate(fft_out){ |real, imag| OpenStruct.new(real: real / n, imaginary: imag / n) }
     end
 
-    def calculate1
-        split_data = split
-        strategy.data = split_data.real
-        real_out = strategy.calculate
-        strategy.data = split_data.imag.map{|val| -val }
-        imag_out = strategy.calculate
-        n = real_out.length.to_f
-        ifft = []
-        for i in 0...real_out.length
-            ifft << Complex(real_out[i] / n, -1 * imag_out[i] / n)
-        end
-        ifft
-    end 
 
     private
 
@@ -41,22 +28,6 @@ class Dsp::Strategies::IFFTConjugateStrategy
                 real_num.real
             end
         end
-    end
-
-    def split
-        split_data = OpenStruct.new
-        split_data.real = []
-        split_data.imag = []
-        data.each do |value|
-            if data.is_a? Complex
-                split_data.real << value.real
-                split_data.imag << value.imaginary
-            else
-                split_data.real << value
-                split_data.imag << 0
-            end
-        end
-        split_data
     end
 
 
