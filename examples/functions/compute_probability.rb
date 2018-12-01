b682283556cac06e219154016813d69958071726
micahshute <micah.shute@gmail.com>
2018-12-01 11:50:07 -0600

    added example showing calculation of bit error

---
+++ examples/functions/compute_probability.rb
@@ -0,0 +1,43 @@
+# eb_no = 10 ** (23.5 / 10)
+# m = 256
+# es_no = eb_no * Math.log(m,2)
+# q = Dsp::Probability.normal_q( Math.sqrt(es_no * 3.0 / 255))
+# smo2 = (Math.sqrt(m) - 2)
+# pc1 = (1 - 2 * q) ** 2
+# pc2 = (1 - 2 * q) * (1 - q)
+# pc3 = (1 - q) ** 2
+# qam_ps = 1 - (1.0 / m) * ((smo2 ** 2) * pc1 + 4 * smo2 * pc2 + 4 * pc3)
+# # qam_ps = 4 * (1 - (1.0 / Math.sqrt(m)) * q)
+# qam_pb = qam_ps / Math.log(m, 2)
+# qam_pb
+
+qam_eqn = Proc.new do |db|
+    peb_no = 10 ** (db / 10)
+    pes_no = peb_no * Math.log(256,2)
+    pq = Dsp::Probability.normal_q( Math.sqrt(pes_no * 3.0 / 255))
+    psmo2 = (Math.sqrt(256) - 2)
+    ppc1 = (1 - 2 * pq) ** 2
+    ppc2 = (1 - 2 * pq) * (1 - pq)
+    ppc3 = (1 - pq) ** 2
+    ps = 1 - (1.0 / 256) * ((psmo2 ** 2) * ppc1 + 4 * psmo2 * ppc2 + 4 * ppc3)
+    ps / Math.log(256, 2)
+end
+
+psk_eqn = Proc.new do |db|
+
+end
+
+def findval(eqn, value, desired_val, step = 0.001 , tolerance = 0.000000005)
+    output = eqn[value]
+    if (output >= (desired_val - tolerance)) and (output <= (desired_val + tolerance))
+        return value
+    else
+        slope = (output <=> eqn[value + tolerance])
+        dir_to_output = output <=> desired_val
+        next_val = value + slope * dir_to_output * step
+        return findval(eqn, next_val, desired_val)
+    end
+end
+
+qam = findval(qam_eqn, 22 , 0.000001)
+puts "QAM Eb/No = #{qam}"
--- examples/modulation_schemes/dpsk_freq_domain.rb
+++ examples/modulation_schemes/dpsk_freq_domain.rb
@@ -2,13 +2,13 @@
 
 # Make a 100-bit stream, make one PSK and one DPSK signal, plot the frequency domians
 
-# signal2 = Dsp::Probability::RandomBitGenerator.new_bitstream.generate(600).split('')
-# psk2 = Dsp::Strategies::PSK.new(modulating_signal: signal2, coding_strategy: nil, carrier_frequency: 10, pulse_length: 0.05)
-# dpsk2 = Dsp::Strategies::PSK.new(modulating_signal: signal2, carrier_frequency: 10, pulse_length: 0.05)
-# puts dpsk2.coded_signal.to_s
-# puts dpsk2.phase_signal.to_s
-# Dsp::QuickPlot.plot(data: psk2.output.to_ds.fft.magnitude, title: "PSK 2")
-# Dsp::QuickPlot.plot(data: dpsk2.output.to_ds.fft.magnitude, title: "DPSK 2")
+signal2 = Dsp::Probability::RandomBitGenerator.new_bitstream.generate(600).split('')
+psk2 = Dsp::Strategies::PSK.new(modulating_signal: signal2, coding_strategy: nil, carrier_frequency: 10, pulse_length: 0.05)
+dpsk2 = Dsp::Strategies::PSK.new(modulating_signal: signal2, carrier_frequency: 10, pulse_length: 0.05)
+puts dpsk2.coded_signal.to_s
+puts dpsk2.phase_signal.to_s
+Dsp::QuickPlot.plot(data: psk2.output.to_ds.fft.magnitude, title: "PSK 2")
+Dsp::QuickPlot.plot(data: dpsk2.output.to_ds.fft.magnitude, title: "DPSK 2")
 
 # Make 4 256-bit symbols, make one PSK and one DPSK signal, plot the frequency domians
 signal256 = Dsp::Probability::RandomBitGenerator.new_symbol_stream(bits_per_symbol: Math.log(256,2).to_i).generate 75
