de7a93aa7331f3e7ff184bfc99a3def11165d0e0
micahshute <micah.shute@gmail.com>
2018-12-01 02:09:36 -0600

    added to gram schmidt example

--- examples/functions/gram_schmidt.rb
+++ examples/functions/gram_schmidt.rb
@@ -1,3 +1,7 @@
 messages = [[1,1], [1,-1], [-1,1]]
 output_vectors = Dsp::Strategies::GramSchmidtOrthonormalize.new(messages).output
 puts output_vectors.to_s
+# Therefore, this signal cannot be reprisented in 3-D Space
+
+#Minimize energy in the 2-D reprisentation
+puts Dsp::Functions.translate_center_to_origin([[1,1],[1,-1],[-1,1]]).to_s
