2f5e7956f6a1b6cdac5e3d7a7d99b40861092a0e
Micah Shute <micah.shute@gmail.com>
2019-03-22 01:52:31 -0500

    add gauss seidel strategy and associated rdoc and tests

--- lib/strategies/linear_algebra/gauss_seidel_strategy.rb
+++ lib/strategies/linear_algebra/gauss_seidel_strategy.rb
@@ -1,16 +1,15 @@
-
 ##
-# This strategy will solve a system of linear equations using the Jacobi iterative strategy
+# This strategy will solve a system of linear equations using the Gauss Seidel iterative strategy
 # Constructor Inputs: (a_arr, b_arr) correspond to A and B in the equation Ax = B (a should be an nxn 2D array, and b should be a 1D array (even though in the equation it is a column vector))
 #
 ## a = [[4,1,-1],[2,7,1],[1,-3,12]]
 ## b = [3,19,31]
-## gs = Dsp::Strategies::JacobiStrategy.new(a,b)
+## gs = Dsp::Strategies::GaussSeidelStrategy.new(a,b)
 ## x = gs.calculate # => Matrix[[0.9998668946614292], [2.000021547671973], [3.000054218557957]]
 
-class Dsp::Strategies::JacobiStrategy
 
-    attr_reader :a, :b, :d, :u, :l, :dinv
+class Dsp::Strategies::GaussSeidelStrategy
+
 
     def initialize(a_arr,b_arr)
         # TODO: Raise exception if a_arr is not square and b_arr row_count != a_arr row count
@@ -40,7 +39,7 @@ class Dsp::Strategies::JacobiStrategy
     end
 
     ##
-    # Iteratively solves the linear system of equations using the jacobi method
+    # Iteratively solves the linear system of equations using the Gauss Seidel method
     # accepts an optional parameter which is the threshold value x(n+1) - x(n) should achieve before returning. 
     # Must be used with a key-value pair ie `gs.calculate(threshold: 0.001)
     # default threshold = 0.001
@@ -54,9 +53,13 @@ class Dsp::Strategies::JacobiStrategy
         x_n = Matrix.column_vector(Array.new(@a.column_count, 0))
         counter = 0
         loop do 
-            x_n_plus_1 = c + t * x_n
+            x_n_plus_1 = x_n.dup
+            for i in 0...x_n.row_count do 
+                x_n_i = c.row(i).to_matrix + t.row(i).to_matrix.transpose * x_n_plus_1
+                # puts "#{c.row(i).to_matrix} + #{t.row(i).to_matrix.transpose} * #{x_n_plus_1} = #{x_n_i}"
+                x_n_plus_1[i,0] = x_n_i[0,0]
+            end
             x_difference = (x_n_plus_1 - x_n).map{ |el| el.abs }
-            # puts x_n_plus_1
             should_break = !x_difference.find{ |el| el > threshold }
             x_n = x_n_plus_1           
             break if should_break
@@ -67,6 +70,7 @@ class Dsp::Strategies::JacobiStrategy
         end 
         return (safety_net and x_n.find{ |el| el.to_f.nan? }) ? (@a.inv * b).map{ |el| el.to_f} : x_n
     end
+
 
 
 end
--- spec/jacobi_strategy_spec.rb
+++ spec/jacobi_strategy_spec.rb
@@ -4,12 +4,33 @@ RSpec.describe Dsp::Strategies::JacobiStrategy do
 
         a = [[4,1,-1],[2,7,1],[1,-3,12]]
         b = [3,19,31]
-        gs = Dsp::Strategies::JacobiStrategy.new(a,b)
-        x = gs.calculate
-        expect(x[0,0] - 1 ).to be < 0.001
-        expect(x[1,0] - 2).to be < 0.001
-        expect(x[2,0] - 3).to be < 0.001
+        js = Dsp::Strategies::JacobiStrategy.new(a,b)
+        x = js.calculate
+        expect((x[0,0] - 1).abs).to be < 0.001
+        expect((x[1,0] - 2).abs).to be < 0.001
+        expect((x[2,0] - 3).abs).to be < 0.001
     end
 
+
+    it "diverges for some linear equations" do
+        a = [[1,2,1],[2,5,2],[1,3,4]]
+        b = [2,1,5]
+        js = Dsp::Strategies::JacobiStrategy.new(a,b)
+        x = js.calculate
+        expect(x[0,0].to_f.nan? ).to eq true
+        expect(x[1,0].to_f.nan? ).to eq true
+        expect(x[2,0].to_f.nan?).to eq true
+    end
+
+    it "has an option to solve using matrix inverses if divergence occurs" do
+        a = [[1,2,1],[2,5,2],[1,3,4]]
+        b = [2,1,5]
+        js = Dsp::Strategies::JacobiStrategy.new(a,b)
+        x = js.calculate(safety_net: true)
+        expect(x[0,0]).to eq 6
+        expect(x[1,0]).to eq -3
+        expect(x[2,0]).to eq 2
+
+    end
 
 end
