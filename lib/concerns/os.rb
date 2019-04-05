7adf8f90a52bf430013aa6b67daf1d32c3a90a7a
Micah Shute <micah.shute@gmail.com>
2019-04-05 01:39:53 -0500

    add rdoc

--- lib/concerns/os.rb
+++ lib/concerns/os.rb
@@ -1,22 +1,34 @@
+## 
+# Deternine the OS for classes which need to perform system commands
 module Dsp::OS
 
 
+    ## 
+    # return true if in a windows env
     def windows?
         (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
     end
     
+    ## 
+    # return true if in a mac env
     def mac?
         (/darwin/ =~ RUBY_PLATFORM) != nil
     end
     
+    ## 
+    # return true if in a unix env
     def unix?
         !windows?
     end
     
+    ## 
+    # return true if in a linux env
     def linux?
         unix? and not mac?
     end
     
+    ## 
+    # return true if using jruby
     def ruby?
         RUBY_ENGINE == 'jruby'
     end
--- lib/probability/gaussian_distribution.rb
+++ lib/probability/gaussian_distribution.rb
@@ -1,3 +1,5 @@
+##
+# Class to create a sample of Gaussian Distributed values
 class Dsp::Probability::GaussianDistribution
 
     attr_accessor :mean, :stddev, :generator, :data
@@ -5,6 +7,12 @@ class Dsp::Probability::GaussianDistribution
 
     include Dsp::Convolvable::InstanceMethods, Dsp::Initializable, Dsp::FourierTransformable
 
+    ##
+    # == Initialize arguments
+    # mean:: [Float] mean of the population
+    # stddev:: [Float] standard deviation of the population
+    # size:: [Integer] number of datapoints
+    # generator:: Strategy for making Gaussian values. Defaults to Dsp::Strategies::GaussianGeneratorBoxMullerStrategy.new
     def initialize(mean: , stddev: , size: ,generator: Dsp::Strategies::GaussianGeneratorBoxMullerStrategy.new)
         @mean, @stddev, @generator, @size = mean, stddev, generator, size
         generator.mean = mean
