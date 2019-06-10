a82546cf24f103ac412d4faa01ce303913a1f0e8
Micah Shute <micah.shute@gmail.com>
2019-06-10 12:18:46 -0500

    fix bundler issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 
 require "bundler/setup"
 
-Bundler.require(:default)
+require 'gruff'
 
 require 'matrix'
 require 'ostruct'
--- lib/digiproc/version.rb
+++ lib/digiproc/version.rb
@@ -1,6 +1,6 @@
 
 module Digiproc
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
 
 
